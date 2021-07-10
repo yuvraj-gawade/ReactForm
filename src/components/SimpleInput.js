26651a92d847e515c98e5ddd9cd5ceb7d6b84af7
Yuvraj IN1076515 <gawadey@vmware.com>
2021-07-10 10:15:21 +0530

    first commit

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,72 @@
+import useInput from "../hooks/use-input.js";
+
+const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput
+  } = useInput((value)  => { return value.trim() !== '' });
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput
+  } = useInput((value)  => { return value.trim() !== '' && value.indexOf('@') !== -1 });
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+    resetNameInput('');
+    resetEmailInput('');
+  };
+
+  const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control';
+  const emailInputClasses = emailInputHasError ? 'form-control invalid' : 'form-control';
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input
+          type='text'
+          id='name'
+          onChange={nameChangeHandler} value={enteredName}
+          onBlur={nameBlurHandler}
+        />
+        { nameInputHasError && <p className="error-text">Name must not be empty</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor='name'>Your Email</label>
+        <input
+          type='text'
+          id='name'
+          onChange={emailChangeHandler} value={enteredEmail}
+          onBlur={emailBlurHandler}
+        />
+        { emailInputHasError && <p className="error-text">Please ented valid email</p>}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
